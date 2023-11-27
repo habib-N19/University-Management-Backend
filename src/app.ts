2884c97e3d98483062b9b317e8405d60e1545f5d
habibur nabi <habiburnabiarafat@gmail.com>
2023-11-27 13:15:01 +0600

    bare minmum server setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to The University Management Tool');
+});
+
+export default app;
